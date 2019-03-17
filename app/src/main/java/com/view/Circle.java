980eb82380fe183be8993b32c9c937062e20cc7e
chaoyuexing <931368239@qq.com>
2019-03-17 18:57:46 +0800

    增加位置的确认

--- app/src/main/java/com/view/Circle.java
+++ app/src/main/java/com/view/Circle.java
@@ -1,10 +1,12 @@
 package com.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 
@@ -14,10 +16,18 @@ import java.util.ArrayList;
 public class Circle extends View {
 
     private String TAG = Circle.class.getName();
-    private int startAngle = 0; // 默认起点角度为0
-    private int sweepAngle = 0;
+    private float startAngle = 0; // 默认起点角度为0
+    private float sweepAngle = 0;
     private int roundAngle = 360;
     private RectF mRectF;
+    private float circleSize;
+    private float left;
+    private float right;
+    private float top;
+    private float bottom;
+
+    private float strokeWidth = 10f;
+
 
     private ArrayList<Integer> mPercentage;
     private ArrayList<Integer> mColors;
@@ -29,6 +39,10 @@ public class Circle extends View {
 
     public Circle(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.Circle);
+        if (ta != null) {
+            circleSize = ta.getDimension(R.styleable.Circle_circle_size,10);
+        }
     }
 
 
@@ -83,16 +97,30 @@ public class Circle extends View {
 
     private void init() {
         Log.e(TAG, "init: ");
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+//        Log.e(TAG, "displayMetrics.widthPixels: "+ displayMetrics.widthPixels);
+//        Log.e(TAG, "displayMetrics.heightPixels: "+ displayMetrics.heightPixels);
         int width = getWidth();
         int height = getHeight();
+//        Log.e(TAG, "width: "+ width);
+//        Log.e(TAG, "height: "+ height);
         int length = Math.min(width, height);
-        mRectF = new RectF(length * 0.1f, length * 0.1f, length * 0.9f,
-                length * 0.9f);
+
+        left = circleSize+getPaddingLeft();
+        top = circleSize + getPaddingBottom();
+        right = length - getPaddingRight() - circleSize;
+        bottom = length - getPaddingBottom() - circleSize;
+        Log.e(TAG, "length: "+ length);
+        Log.e(TAG, "left: "+ left);
+        Log.e(TAG, "top: "+ top);
+        Log.e(TAG, "right: "+ right);
+        Log.e(TAG, "bottom: "+ bottom);
+        mRectF = new RectF(left  , top , right, bottom);
         for (int i = 0; i < this.mPercentage.size(); i++) {
             Paint mArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             mArcPaint.setColor(this.mColors.get(i));
             mArcPaint.setStyle(Paint.Style.STROKE);
-            mArcPaint.setStrokeWidth((width * 0.1f));
+            mArcPaint.setStrokeWidth(circleSize);
             mPaints.add(mArcPaint);
         }
     }
--- app/src/main/java/com/view/MainActivity.java
+++ app/src/main/java/com/view/MainActivity.java
@@ -1,13 +1,10 @@
 package com.view;
 
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewParent;
 import android.widget.LinearLayout;
 
@@ -19,21 +16,31 @@ public class MainActivity extends AppCompatActivity {
     private Circle mCircle;
     private ArrayList<Integer> baifenbi = new ArrayList<>();
     private ArrayList<Integer> colors = new ArrayList<>();
+    private String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DisplayMetrics metrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        // 屏幕的分辨率
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        Log.e(TAG, "width: "+ width);
+        Log.e(TAG, "height: "+ height);
+
         mainLayout = findViewById(R.id.main_layout);
         mCircle = findViewById(R.id.mCircle);
-        baifenbi.add(10);
-        baifenbi.add(20);
         baifenbi.add(40);
         baifenbi.add(30);
-        colors.add(Color.BLACK);
-        colors.add(Color.BLUE);
+        baifenbi.add(20);
+        baifenbi.add(10);
         colors.add(Color.YELLOW);
         colors.add(Color.RED);
+        colors.add(Color.BLUE);
+        colors.add(Color.BLACK);
         mCircle.setData(baifenbi,colors);
         ViewParent viewParent = mainLayout.getParent();
         Log.i("TAG", "the parent of mainLayout is " + viewParent);
